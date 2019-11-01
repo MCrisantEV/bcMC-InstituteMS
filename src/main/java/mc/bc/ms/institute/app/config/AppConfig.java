792c299589aa665028f862cf8358d863988490ac
MCrisantEV <mcrisant@everis.com>
2019-11-01 13:35:19 -0500

    Agregando la configuracion de los puertos del Config Server Version v0.5.1

--- src/main/java/mc/bc/ms/institute/app/config/AppConfig.java
+++ src/main/java/mc/bc/ms/institute/app/config/AppConfig.java
@@ -1,5 +1,6 @@
 package mc.bc.ms.institute.app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class AppConfig {
 	
+	@Value("${coursePort:8002}")
+    private String coursePort;
+	
 	@Bean
 	public WebClient createWebClient() {
-		return WebClient.create("http://localhost:8002/courses");
+		return WebClient.create("http://localhost:"+coursePort+"/courses");
 	}
 }
